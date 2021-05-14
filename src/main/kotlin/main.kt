d563bd9430510dd63b311b33c8841cfbca704f92
Pablo Lago Castro <plagocastro@danielcastelao.org>
2021-05-14 09:13:59 +0200

    Ejerciccio 1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,11 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val tamaño: Int = 10
+    fun array() {
+        for (it in 0..10) {
+            val tabla1 = Array<Int>(tamaño) { (it * 6) }
+
+        }
+        println("Tabla de multiplicar ${array()}")
+    }
+
 }
