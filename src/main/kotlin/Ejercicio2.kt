38b50d99843faa3d75a0d3be048a5832035ce79f
plagocastro <72438961+plagocastro@users.noreply.github.com>
2021-05-14 10:15:12 +0200

    Borramos la clase sobrante

--- src/main/kotlin/Ejercicio2.kt
+++
@@ -1,8 +0,0 @@
-class Ejercicio2 (var tamaño:Int){
-    fun TablaD6(){
-        tamaño = 11
-        val tablaMul = Array<Int>(tamaño) { it * 6 }.forEach {
-            print("6*${it / 6} = ${it}")
-        }
-    }
-}
