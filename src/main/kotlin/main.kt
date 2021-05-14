9201907c7288c6a9d17bedba7097e01e7f4d8cc3
Pablo Lago Castro <plagocastro@danielcastelao.org>
2021-05-14 10:01:28 +0200

    Division en clases

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,13 +4,3 @@ fun main(args: Array<String>) {
     }
 
 
-
-class array(var tamaño:Int){
-    fun TablaD6(){
-        tamaño = 11
-        val tablaMul = Array<Int>(tamaño) { it * 6 }.forEach {
-            print("6*${it / 6} = ${it}")
-        }
-    }
-
-}
