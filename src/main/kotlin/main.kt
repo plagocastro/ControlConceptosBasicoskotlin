0551e77860af6b41a98536a65ee5b1503410d998
Pablo Lago Castro <plagocastro@danielcastelao.org>
2021-05-14 10:02:08 +0200

    Division en clases2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
     val array = array(10)
-    println("La tabla del 6 será")
+    println("La tabla del 6 será..")
     array.tablaMul.forEach{
         print("6*${it/6} = ${it}")
 
